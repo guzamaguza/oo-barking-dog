b5df0dcedce05f1bebe278a3f2398c30b8914eb1
guzamaguza <57208488+guzamaguza@users.noreply.github.com>
2019-12-08 11:57:20 -0800

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Dog
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+
+  #setter method
+  def name = (name)
+    @name =  name
+  end
+
+  #getter method
+  def name
+    @name
+  end
+
+  def bark
+    puts "woof!"
+  end
+
+end
